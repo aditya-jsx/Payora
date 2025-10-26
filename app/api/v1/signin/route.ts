0b89a313984e187678cd873f8d9d43703261d1f9
Aditya Shrivastav <devs.aadi20@gmail.com>
2025-10-27 03:51:53 +0530

    added prisma, created the schema of user table, made signup and signin endpoints

---
+++ app/api/v1/signin/route.ts
@@ -0,0 +1,84 @@
+import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
+import bcrypt from "bcrypt";
+import client from "@/app/lib/db"
+import { JWT_SECRET } from "@/app/lib/config";
+import jwt from "jsonwebtoken"
+
+export async function POST(req:NextRequest) {
+    const body = await req.json();
+    const requiredBody = z.object({
+        username: z.string(),
+        password: z
+            .string()
+            .min(8, "Password must contain atleast 8 characters")
+    });
+    const parsedData = requiredBody.safeParse(body);
+    if(!parsedData.success){
+        return NextResponse.json({
+            msg: "Invalid Format",
+            error: parsedData.error.flatten().fieldErrors
+        }, {
+            status: 400
+        });
+    }
+    const { username, password } = parsedData.data;
+    try{
+        const user = await client.user.findFirst({where: {username: username}})
+        if(!user){
+            return NextResponse.json({
+                msg: "Invalid username or Password"
+            }, {
+                status: 403
+            })
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if(passwordMatch){
+            if(!JWT_SECRET){
+                console.error("JWT secret is not configured");
+                return NextResponse.json({
+                    msg: "Server Configuration error"
+                }, {
+                    status: 500
+                })
+            }
+
+            const token = jwt.sign({
+                userId: user.id.toString()
+            }, JWT_SECRET, {
+                expiresIn: '1h'
+            });
+
+            const response = NextResponse.json({msg: "User logged in successfully"});
+
+            response.cookies.set("token", token, { httpOnly: true, secure: process.env.NODE_ENV === 'production', maxAge: 60 * 60, sameSite: 'lax', path: "/" });
+            
+            return response;
+        }else{
+            return NextResponse.json({
+                msg: "Invalid username or password"
+            }, {
+                status: 403
+            });
+        }
+    }catch(e){
+        console.error("Error during signin", e);
+        if (e instanceof Error) {
+            return NextResponse.json({
+                msg: "An unexpected error occurred during signin",
+                error: e.message 
+            }, {
+                status: 500
+            });
+        }else{
+            return NextResponse.json({
+                msg: "An unexpected and unknown error occurred",
+                error: String(e)
+            }, {
+                status: 500
+            });
+        }
+    }
+}
---
+++ app/api/v1/signup/route.ts
@@ -0,0 +1,69 @@
+import { NextRequest, NextResponse } from "next/server";
+import z from "zod";
+import bcrypt from "bcrypt";
+import client from "@/app/lib/db"
+
+export async function POST(req:NextRequest) {
+    const body = await req.json();
+    const requiredBody = z.object({
+        username: z.string(),
+        firstName: z.string(),
+        lastName: z.string(),
+        password: z
+            .string()
+            .min(8, "Password must contain atleast 8 characters")
+            .regex(/[A-Z]/, "Must contain an uppercase letter")
+            .regex(/[a-z]/, "Must contain an lowercase letter"),
+    });
+    const parsedData = requiredBody.safeParse(body);
+    if(!parsedData.success){
+        return NextResponse.json({
+            msg: "Invalid Format",
+            error: parsedData.error.flatten().fieldErrors
+        }, {
+            status: 400
+        });
+    }
+    const { username, firstName, lastName, password } = parsedData.data;
+    try{
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const existingUser = await client.user.findFirst({where: {username: username}});
+        if(existingUser){
+            return NextResponse.json({
+                msg: "User with this username already exists"
+            }, {
+                status: 400
+            })
+        }
+        await client.user.create({
+            data: {
+                username: username,
+                firstName: firstName,
+                lastName: lastName,
+                password: hashedPassword
+            }
+        })
+        return NextResponse.json({
+            msg: "Singup Completed Successfully!"
+        }, {
+            status: 200
+        })
+    }catch(e){
+        console.error("Database error occured during signup", e);
+        if (e instanceof Error) {
+            return NextResponse.json({
+                msg: "An unexpected error occurred during signup",
+                error: e.message 
+            }, {
+                status: 500
+            })
+        }else{
+            return NextResponse.json({
+                msg: "An unexpected and unknown error occurred",
+                error: String(e)
+            }, {
+                status: 500
+            });
+        }
+    }
+}
