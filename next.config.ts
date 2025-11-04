4daf91da6233c46784730c12da8611e0cd28beac
Aditya Shrivastav <devs.aadi20@gmail.com>
2025-11-05 01:44:20 +0530

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "https://payora-bice.vercel.app" },
+
+          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS" },
+
+          { key: "Access-Control-Allow-Headers", value: "Content-Type, Authorization, user-id" },
+
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
